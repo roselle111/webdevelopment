ba7aa6ba1689c2add842c4e8c6d15939826090d5
roselle <22104644@usc.edu.ph>
2023-10-12 19:43:12 +0800

    update

--- dataphp/manageprocess.php
+++ dataphp/manageprocess.php
@@ -10,15 +10,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Perform validation if needed
 
     // Insert data into the database
-    $sql = "INSERT INTO activities (status, remarks) VALUES ('$status', '$remarks')";
+    $sql = "INSERT INTO manage (status, remarks) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $status, $remarks);
 
-    if ($conn->query($sql) === TRUE) {
+    if ($stmt->execute()) {
         header('Location: ../life.html');
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $stmt->error;
     }
+
+    // Close the statement
+    $stmt->close();
 }
 
 // Close the database connection
 $conn->close();
-?>
+
+
+
+
+
+
+
