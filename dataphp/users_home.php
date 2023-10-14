e71aabaaf534cfd11a839422b16b141cd2e4bcb2
roselle <22104644@usc.edu.ph>
2023-10-14 15:50:07 +0800

    update

--- dataphp/users_home.php
+++ dataphp/users_home.php
@@ -1,49 +1,77 @@
-<?php
-include_once('../includes/userSessions.php');
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        /* Basic styling for the navbar */
-        .navbar {
-            overflow: hidden;
-            background-color: #333;
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
         }
 
-        /* Style for the navbar links */
+        .navbar {
+            background-color: #333;
+            overflow: hidden;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+
         .navbar a {
             float: left;
-            font-size: 16px;
+            display: block;
             color: white;
             text-align: center;
             padding: 14px 16px;
             text-decoration: none;
+            transition: background-color 0.3s, color 0.3s;
         }
 
         .navbar a.right {
             float: right;
         }
 
-        /* Change color on hover */
         .navbar a:hover {
-            background-color: #ddd;
-            color: black;
+            background-color: #555;
+            color: #fff;
+        }
+
+        .content {
+            margin: 20px;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+            transition: transform 0.3s;
+        }
+
+        .content:hover {
+            transform: scale(1.02);
+        }
+
+        h1 {
+            color: #333;
+            transition: color 0.3s;
+        }
+
+        .content:hover h1 {
+            color: #007bff;
         }
     </style>
 </head>
-
 <body>
+
     <div class="navbar">
-        <!-- Links for managing life activities -->
         <a href="../life.html">Add Activity</a>
         <a href="edit_activity.php">Edit Activity</a>
         <a href="logout.php" class="right">Logout</a>
     </div>
+
     <div class="content">
         <h1>Welcome to User Home Page</h1>
     </div>
-</body>
 
+</body>
 </html>
